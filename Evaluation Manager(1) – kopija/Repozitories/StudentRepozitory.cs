9397377e4c295f7ba69598ab448f5b03bd724a0b
mkelemen <mkelemen21@student.foi.hr>
2023-05-02 20:47:21 +0200

    Kreiran student repozitrorij

---
+++ Evaluation Manager(1) – kopija/Repozitories/StudentRepozitory.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Evaluation_Manager.Repozitories {
+    internal class StudentRepozitory {
+    }
+}
